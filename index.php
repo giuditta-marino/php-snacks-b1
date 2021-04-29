7e9d5ec14a4a7829d2257591297e7ccc72dc5bad
Giuditta <giudittamarino89@gmail.com>
2021-04-29 16:25:34 +0200

    snack 2 terminato

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<!-- PHP SNACK 1 -->
+
 <!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del
 calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti
 dalla squadra di casa e punti fatti dalla squadra ospite.
@@ -51,4 +53,41 @@ for ($i=0; $i < count($partite); $i++) {
    <?= $partite[$i]['puntiCasa'] ?> -
    <?= $partite[$i]['puntiOspite'] ?>
  </div>
+<?php } ?>
+
+<!-- PHP SNACK 2 -->
+<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi
+che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri,
+che mail contenga un punto e una chiocciola e che age sia un numero.
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+<?php
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+
+// var_dump($name, $mail, $age);
+
+$resultName = strlen($name) >=3;
+$resultMailAt = strpos($mail, '@');
+$resultMailDot = strpos($mail, '.');
+$resultAge = is_numeric($age);
+
+var_dump($resultName, $resultMailAt, $resultMailDot, $resultAge);
+
+// if (!$resultName || !$resultMailAt || !$resultMailDot || !$resultAge) {
+//   echo 'ko';
+// } else {
+//   echo 'ok';
+// }
+
+if ($resultName === true & $resultMailAt == true & $resultMailDot == true & $resultAge === true) { ?>
+   <div class="">
+     Accesso riuscito
+   </div>
+   <?php
+} else { ?>
+  <div class="">
+    Accesso negato
+  </div>
 <?php } ?>
